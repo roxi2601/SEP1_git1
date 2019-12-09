3474f79b0ae762e2886e46e8eb37835bb12b212e
roxi2601 <roxi260111@gmail.com>
2019-12-09 18:33:51 +0100

    update

--- SEP_git2/src/ChangeDateTab.java
+++ SEP_git2/src/ChangeDateTab.java
@@ -11,50 +11,10 @@ import javafx.scene.layout.VBox;
 
 /**
  * A GUI tab containing components for changing a exam's date.
- * @author Roksana Dziadowicz
+ * @author
  * @version 1.0
  */
-public class ChangeDateTab
+public class ChangeDateTab extends Tab
 {
-  private HBox changeDatePane; //all
 
-  private HBox addAndRemoveButtons;
-
-  private VBox coursePane; // left
-  private VBox datePane; //right
-
-  private ComboBox<Exam> examBox;
-  private ComboBox<Room> dateBox;
-
-  private GridPane changeDateInputPane; //right
-  private GridPane courseDataPane; //left
-
-  private Button addButton;
-  private Button removeButton;
-
-  private TextField courseField;
-  private TextField examinerField;
-  private TextField roomField;
-  private TextField dateField;
-  private TextField dayField;
-  private TextField monthField;
-  private TextField yearField;
-
-  private Label courseLabel;
-  private Label examinerLabel;
-  private Label roomLabel;
-  private Label dateLabel;
-  private Label dayLabel;
-  private Label monthLabel;
-  private Label yearLabel;
-
-  private MyActionListener listener;
-
-  private ExamScheduleAdapter adapter;
-
-  /**
-   * Constructor initializing the GUI components
-   * @param title The title of the tab
-   * @param adapter ExamScheduleAdapter object used for retrieving and storing date information
-   */
 }
